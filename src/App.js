52a39b56f022e99389f5a6d6d01b231387148dc4
Aleksandra Karabasevic <avax88@hotmail.com>
2020-11-20 10:38:21 +0100

    recovery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import './style.css';
-import SearchMovies from './SearchMovies'
+import React, { useEffect } from "react";
+import "./style.css";
+import SearchMovies from "./SearchMovies";
 
 function App() {
+  useEffect(() => {
+    fetch(".netlify/functions/lambda")
+      .then((response) => response.json.toString())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div className="container">
       <h1 className="title">Find a movie</h1>
-      <SearchMovies/>
+      <SearchMovies />
     </div>
   );
 }
--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,41 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./style.css";
-import MovieCard from './MovieCard';
+import MovieCard from "./MovieCard";
 
 export default function SearchMovies() {
-    const [query, setQuery] = useState('');
-    const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
 
-    const searchMovies = async (e) => {
-        e.preventDefault()
-        const api_key=process.env.REACT_APP_API_KEY;
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${query}&page=1&include_adult=false`;
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    const api_key = process.env.REACT_APP_API_KEY;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${query}&page=1&include_adult=false`;
 
-        try {
-            const res = await fetch(url);
-            const data = await res.json();
-            // console.log(data.results);
-            setMovies(data.results)
-        } catch (err) {
-            console.error(err);
-        }
-
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      //   console.log(data.results);
+      setMovies(data.results);
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    return (
-        <>
-            <form className="form" onSubmit={searchMovies}>
-                <label className="label" htmlFor="query">Movie name</label>
-                <input className="input" type="text" name="query"
-                    placeholder="e.g. Ice age"
-                    value={query} onChange={(e) => setQuery(e.target.value)} />
-                <button type="submit" className="button">Search</button>
-            </form>
-            <div className='card-list'>
-                {movies.filter(movie => movie.poster_path).map(movie => (
-                    <MovieCard movie={movie} key={movie.id} />
-                ))}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="e.g. Ice age"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="button">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </>
+  );
 }
